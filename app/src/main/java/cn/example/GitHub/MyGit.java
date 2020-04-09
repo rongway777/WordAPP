0befebbf9bb24129597d513d188d095847f0894b
rongway777 <1254018113@qq.com>
2020-04-09 17:46:19 +0800

    单词APP Git测试。

---
+++ app/src/main/java/cn/example/GitHub/MyGit.java
@@ -0,0 +1,25 @@
+package cn.example.GitHub;
+
+public class MyGit {
+
+    private String name = "闫康";
+    private String password = "92314000...aaa";
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
